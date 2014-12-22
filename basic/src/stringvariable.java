a310dc8567d7adb1f67410dea0c7e607dc6b6cc7
MohamedZajith <mohamedzajith@gmail.com>
2014-12-23 00:39:54 +0530

    string variable

---
+++ basic/src/stringvariable.java
@@ -0,0 +1,13 @@
+/**
+ * Created by hp on 12/23/14.
+ */
+public class stringvariable {
+    public static void main(String[] args) {
+        String name="mohamedzajith";
+
+        System.out.println("my name is "+name);
+        
+        String s1=new String("nimishan");
+        System.out.println("your name is " + s1);
+    }
+}
