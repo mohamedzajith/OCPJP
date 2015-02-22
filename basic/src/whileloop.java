0a2140a596f88181c9e90b1c5abc6d7e24b4d912
MohamedZajith <mohamedzajith@gmail.com>
2015-02-22 12:25:16 +0530

    While Loops in example and oracle link more details

---
+++ basic/src/whileloop.java
@@ -0,0 +1,16 @@
+/**
+ * Created by hp on 2/22/15.
+ */
+public class whileloop {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count < 11) {
+            System.out.println("Count is: " + count);
+            count++;
+        }
+    }
+    /*
+    http://docs.oracle.com/javase/tutorial/java/nutsandbolts/while.html
+     */
+
+}
