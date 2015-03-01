7bf02472b2616a5eb52d1f93f641ff9dcbe126cc
MohamedZajith <mohamedzajith@gmail.com>
2015-03-01 21:10:22 +0530

    MathOperator methods example

---
+++ adv/src/mathOperation.java
@@ -0,0 +1,14 @@
+/**
+ * Created by dba on 3/1/15.
+ */
+public class mathOperation {
+    public static void main(String[] args) {
+        System.out.println("Math.ceil(9.123)-->"+Math.ceil(9.123));
+        System.out.println("Math.floor(6.5653)-->"+Math.floor(6.5653));
+        System.out.println("Math.max(42.596,45.233)-->"+Math.max(42.596,45.233));
+        System.out.println("Math.min(42.596,45.233)-->"+Math.min(42.596, 45.233));
+        System.out.println("Math.pow(5,3)-->"+Math.pow(5,3));
+        System.out.println("Math.sqrt(213.12)-->"+Math.sqrt(213.12));
+        
+    }
+}
