329a4d89d428c5b92a15d9d2f445e34d77334a51
MohamedZajith <mohamedzajith@gmail.com>
2015-05-20 11:55:52 +0530

    config for loop {n/2 ---> Math.sqrt(n)} Math.sqrt(n) its working .....

--- PrimeNumber/src/com/mohamed/primenumber/checkprime.java
+++ PrimeNumber/src/com/mohamed/primenumber/checkprime.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  */
 public class checkprime {
     public boolean prime(int num){
-        for (int i=2;i<=num/2;i++){
+        for (int i=2;i<=Math.sqrt(num);i++){
             if (num%i==0){
                 return false;
             }
